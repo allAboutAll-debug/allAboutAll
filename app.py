9947f74a161e957cc33defba3568560ffb47051d
aboutBetweenAll <aboutBetweenAll@yandex.ru>
2020-01-15 15:25:32 +0300

    eleven

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ def send_text(message):
             'telegram_id': message.from_user.id,
             'first_name': message.from_user.first_name
         }
-
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
             'Content-Type':'application/json',
