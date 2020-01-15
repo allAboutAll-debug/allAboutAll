cc49e4bc8551e1e1f9b7b951b1307dc77c3dede6
aboutBetweenAll <aboutBetweenAll@yandex.ru>
2020-01-15 15:29:58 +0300

    12

--- app.py
+++ app.py
@@ -10,23 +10,12 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text == '/menu':
-        # url = 'http://aboutbetweenall.000webhostapp.com/api/user/read.php'
-        # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        # result = requests.get(url, headers=headers)
-        # print(result.content.decode())
-
         url = 'http://aboutbetweenall.000webhostapp.com/api/user/create.php'
         payload = {
             'telegram_id': message.from_user.id,
             'first_name': message.from_user.first_name
         }
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
-            'Content-Type':'application/json',
-            "Accept": "application/json"
-        }
-        s = requests.Session()
-        s.post(url, json=payload, headers=headers)
+        requests.post(url, json=payload)
 
         text = 'Чтобы начать получать уведомления нужно сделать подписку. Здесь же вы можете управлять существующими подписками. (Потребуется подтвердить переход по ссылке)'
         keyboard = telebot.types.InlineKeyboardMarkup()
